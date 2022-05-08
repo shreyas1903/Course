36e96e4f3313af18454ff4c347266546f9fbe3ec
shreyas patil <shreyaspatil1901@gmail.com>
2022-05-08 14:38:14 +0530

    Dom

--- Course/Javascript/DOM selectors/script.js
+++ Course/Javascript/DOM selectors/script.js
@@ -2,29 +2,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-function inputLength() {
+
+function inputLength(){
 	return input.value.length;
 }
 
-function createListElement() {
+function createListElement(){
+	
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
+button.addEventListener("click", function() {
+	if(inputLength() > 0){
 		createListElement();
 	}
-}
+	
+})
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
+input.addEventListener("keypress", function(event) {
+	if(inputLength() > 0 && event.which === 13){
 		createListElement();
 	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+	
+})
